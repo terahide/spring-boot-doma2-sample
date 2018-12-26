ab22fb325ba708a2107e1652552cfd92ab10e076
terahide <hideki.terashima@bigtreetc.com>
2018-12-26 13:41:57 +0900

    Step 2 add web layer

--- sample-web-front/src/main/java/com/sample/web/front/controller/home/HomeController.java
+++ sample-web-front/src/main/java/com/sample/web/front/controller/home/HomeController.java
@@ -1,5 +1,10 @@
 package com.sample.web.front.controller.home;
 
+import com.sample.domain.dto.arigato.Arigato;
+import com.sample.domain.dto.common.Page;
+import com.sample.domain.dto.common.Pageable;
+import com.sample.domain.service.arigato.ArigatoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +14,15 @@ import com.sample.web.base.controller.html.AbstractHtmlController;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
+
 @Controller
 @RequestMapping(path = { "/", "/home" })
 @Slf4j
 public class HomeController extends AbstractHtmlController {
+
+    @Autowired
+    ArigatoService arigatoService;
 
     @Override
     public String getFunctionName() {
@@ -26,6 +36,8 @@ public class HomeController extends AbstractHtmlController {
      */
     @GetMapping
     public String index(Model model) {
+        Page<Arigato> page = arigatoService.search(Pageable.NO_LIMIT);//TODO Fake It
+        model.addAttribute("page", page);
         return "home/index";
     }
 }
